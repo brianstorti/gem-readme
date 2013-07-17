4710ecbb74c2d454f9818beabe9b00c4bbf59908
Brian Thomas Storti <btstorti@gmail.com>
2013-07-17 20:19:10 -0300

    remove useless options

--- lib/rubygems/commands/readme_command.rb
+++ lib/rubygems/commands/readme_command.rb
@@ -2,27 +2,18 @@ class Gem::Commands::ReadmeCommand < Gem::Command
   include Gem::VersionOption
 
   def initialize
-    super 'readme', 'Open the README file of a given gem',
-      command: nil,
-      version: Gem::Requirement.default,
-      latest: false,
-      all: false,
-      editor: ENV['EDITOR'] || "less"
+    super("readme",
+          "Open the README file of a given gem",
+          latest: true,
+          version: Gem::Requirement.default,
+          editor: ENV['EDITOR'] || "less")
 
     add_version_option
   end
 
-  def arguments
-    "GEMNAME"
-  end
-
-  def usage
-    "#{program_name} GEMNAME"
-  end
-
   def execute
     gem_name, _ = options[:args]
-    gem_specification = Gem::Specification.each.select { |spec| spec.name == gem_name }.last
+    gem_specification = Gem::Specification.each.detect { |spec| spec.name == gem_name }
 
     if gem_specification.nil?
       say "Gem '#{gem_name}' not found."
@@ -30,9 +21,6 @@ class Gem::Commands::ReadmeCommand < Gem::Command
     end
 
     gem_path = gem_specification.full_gem_path
-
-    cmd = "#{options[:editor]} $(find #{gem_path} -type f -iname 'readme.*')"
-    say cmd
-    system cmd
+    system "#{options[:editor]} $(find #{gem_path} -type f -iname 'readme.*')"
   end
 end
