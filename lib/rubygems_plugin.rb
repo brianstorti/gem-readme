f1605ce3df6947d61608134e3cb6521701fdc80f
Brian Thomas Storti <btstorti@gmail.com>
2013-07-17 21:24:43 -0300

    remove useless require

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,6 +1,5 @@
 require "rubygems/version"
 require 'rubygems/command_manager'
-require 'rubygems/version_option'
 require 'rubygems/gem/specification'
 
 Gem::CommandManager.instance.register_command :readme
