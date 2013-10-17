0d89e4a901951eb13df77985f5c849a210e72764
Hiroyuki Nakamura <nakamura@spiber.jp>
2013-10-17 22:02:02 +0900

    Render markdown and rdoc as roff.
    if README is markdown or rdoc, it convert markdown/rdoc to roff with
    [ronn](https://github.com/rtomayko/ronn) and show like manpage.

--- lib/rubygems/commands/readme_command.rb
+++ lib/rubygems/commands/readme_command.rb
@@ -1,3 +1,9 @@
+require "ronn"
+require "rdoc"
+require "ronn/roff"
+#
+# Add `readme` command to show gem's README.
+#
 class Gem::Commands::ReadmeCommand < Gem::Command
   def initialize
     super("readme",
@@ -10,11 +16,94 @@ class Gem::Commands::ReadmeCommand < Gem::Command
 
     if gem_specification.nil?
       say "Gem not found."
-      raise Gem::SystemExitException, 1
+      fail Gem::SystemExitException, 1
     end
 
     gem_path = gem_specification.full_gem_path
-    system "#{options[:editor]} $(find #{gem_path} -type f -iname 'readme*' -depth 1)"
+    readme = Dir.glob(File.join(gem_path, "readme*"), File::FNM_CASEFOLD).first
+
+    show readme if readme
+  end
+
+  private
+
+  def show(path)
+    case readme_format path
+    when :markdown
+      show_markdown path
+    when :rdoc
+      show_rdoc path
+    else
+      show_plain path
+    end
+  end
+
+  def show_markdown(path)
+    date = File.ctime(path)
+    doc = Ronn::Document.new(path, {"date" => date})
+    show_roff doc.convert("roff")
+  end
+
+  def show_rdoc(path)
+    date = File.ctime(path)
+    rdoc = ROFFHtmlMarkup.new(RDoc::Options.new)
+    source = File.read(path)
+    doc  = rdoc.parse(source)
+    heading = doc.select{|item| item.respond_to?(:level) && item.level == 1}.first
+    name, section, tagline = sniff(heading.text)
+    show_roff create_roff(rdoc.convert(source),
+                          name, section, tagline, date: date)
+  end
+
+  def sniff(heading)
+    case heading
+    when /([\w_.\[\]~+=@:-]+)\s*\((\d\w*)\)\s*-+\s*(.*)/
+      [$1, $2, $3]
+    when /([\w_.\[\]~+=@:-]+)\s+-+\s+(.*)/
+      [$1, nil, $3]
+    else
+      [nil, nil, $1]
+    end
+  end
+
+  def show_roff(text)
+    groff = "groff -Wall -mtty-char -mandoc -Tascii"
+    rd, wr = IO.pipe
+    if pid = fork
+      rd.close
+    else
+      wr.close
+      STDIN.reopen rd
+      exec "#{groff} | #{options[:editor]}"
+    end
+    wr.puts(text)
+    if pid
+      wr.close
+      Process.wait
+    end
+  end
+
+  def show_plain(path)
+    system "#{options[:editor]} #{path}"
+  end
+
+  # Create roff from html
+  def create_roff(html, name, section, tagline,
+                  manual: nil, version: nil, date: nil)
+    html = html.gsub(/<\/{0,1}(hr).+?>/, '')
+    Ronn::RoffFilter.new(html, name, section, tagline,
+                         manual, version, date).to_s
+  end
+
+  def readme_format(path)
+    case File.extname(path)
+    when ".md", ".markdown", ".mdown"
+      :markdown
+    when ".rdoc"
+      :rdoc
+    else
+      :other
+    end
   end
 
   def get_gem_specification
@@ -26,7 +115,6 @@ class Gem::Commands::ReadmeCommand < Gem::Command
 
     ask_for_specific_version(specifications)
   end
-  private :get_gem_specification
 
   def ask_for_specific_version(gem_specifications)
     say("Choose a version:")
@@ -38,5 +126,14 @@ class Gem::Commands::ReadmeCommand < Gem::Command
     index = STDIN.getc.to_i
     gem_specifications[index]
   end
-  private :ask_for_specific_version
+
+  class ROFFHtmlMarkup < RDoc::Markup::ToHtml
+    def accept_heading heading
+       level = [6, heading.level].min
+       @res << "\n<h#{level}>"
+       @res << to_html(heading.text)
+       @res << "</h#{level}>\n"
+    end
+  end
+
 end
